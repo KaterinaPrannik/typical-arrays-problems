b35ba33fcb8d9e1d6dcd8dc9f247b5bf0b8706f3
KaterinaPrannik <60596523+KaterinaPrannik@users.noreply.github.com>
2020-03-09 20:19:31 +0300

    fix: add decide

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
+function min(arr) {
+	if (arguments.length == 0) {
+		return 0;
+	}
+	let n = arr.length;
+	if (n == 0) {
+		return 0;
+	}
 
-exports.min = function min (array) {
-  return 0;
+	let minEl = arr[0];
+	for (let i = 0; i < n; i++) {
+		if (minEl > arr[i]) {
+			minEl = arr[i];
+		}
+
+	}
+	return minEl;
 }
 
-exports.max = function max (array) {
-  return 0;
+function max(arr) {
+	if (arguments.length == 0) {
+		return 0;
+	}
+	let n = arr.length;
+	if (n == 0) {
+		return 0;
+	}
+	let maxEl = arr[0];
+
+	for (let i = 0; i < n; i++) {
+		if (maxEl < arr[i]) {
+			maxEl = arr[i];
+		}
+	}
+	return maxEl;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+function avg(arr) {
+	let avgSum = 0;
+	let avgEl;
+
+	if (arguments.length == 0) {
+		return 0;
+	}
+	let n = arr.length;
+	if (n == 0) {
+		return 0;
+	}
+
+	for (let i = 0; i < n; i++) {
+		avgSum = avgSum + arr[i];
+		avgEl = avgSum / n;
+	}
+	return avgEl;
 }
+
+let arr = [1, 32]
+let minElement = min();
+let maxElement = max(arr);
+let avgElement = avg(arr);
+
+console.log(minElement);
+console.log(maxElement);
+console.log(avgElement);
